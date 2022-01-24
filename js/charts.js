bf687221ad2050f1242a5dd593f3aebbc43d566b
mahasulaiman <mhao_h@hotmail.com>
2022-01-24 12:32:51 +0300

    add js

--- js/charts.js
+++ js/charts.js
@@ -23,7 +23,7 @@ const myChart = new Chart(ctx, {
                 'rgba(255, 99, 132, 1)',
                 'rgba(255, 99, 132, 1)'
             ],
-            borderWidth: 5
+            borderWidth: 3
         }]
     },
     options: {
@@ -35,7 +35,7 @@ const myChart = new Chart(ctx, {
     }
 });
 const ctx1 = document.getElementById('myChart1').getContext('2d');
-const myChart1 = new Chart(ctx, {
+const myChart1 = new Chart(ctx1, {
     type: 'bar',
     data: {
         labels: ['Age1', 'Age3', 'Age5', 'Age7', 'Age9', 'Age11',
@@ -59,7 +59,43 @@ const myChart1 = new Chart(ctx, {
                 'rgba(255, 99, 132, 1)',
                 'rgba(255, 99, 132, 1)'
             ],
-            borderWidth: 5
+            borderWidth: 3
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+const ctx2 = document.getElementById('myChart2').getContext('2d');
+const myChart2 = new Chart(ctx2, {
+    type: 'pie',
+    data: {
+        labels: ['Age1', 'Age3', 'Age5', 'Age7', 'Age9', 'Age11',
+    'Age13'],
+        datasets: [{
+            // label: '# of Votes',
+            data: [0, 10000, 20000, 30000, 40000],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(255, 99, 132, 1)'
+            ],
+            borderWidth: 3
         }]
     },
     options: {
